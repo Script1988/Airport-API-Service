165af0dcd60ea6874a46f9120e197dd7f463298e
Volodymyr Panov <vpanov25@gmail.com>
2023-10-19 13:24:02 +0200

    Created Crew and Airport models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    closest_big_city = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Airport name: {self.name}"
