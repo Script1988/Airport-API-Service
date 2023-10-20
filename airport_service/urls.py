f0f24e2044bd3eed59be91ed41205ea65581599d
Volodymyr Panov <vpanov25@gmail.com>
2023-10-20 12:16:51 +0200

    Downloading home page by default

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("", include("airport.urls", namespace="airport")),
+    path("user/", include("user.urls", namespace="user")),
 ]
