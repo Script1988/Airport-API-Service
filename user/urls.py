d90c6b0dc879d36e5b9b5cd785ac4d29433fc122
Volodymyr Panov <vpanov25@gmail.com>
2023-10-20 07:48:56 +0200

    Added urls for aiport, user and airport_service projects

--- user/urls.py
+++ user/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="obtain_token"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
 ]
 
 app_name = "user"
